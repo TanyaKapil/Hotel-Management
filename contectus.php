18d0cbf0cb24fb8cf1a7443502b0c1f49544017c
Tanya kapil <108415033+TanyaKapil@users.noreply.github.com>
2022-10-19 18:22:04 +0530

    Add files via upload

---
+++ contectus.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Untitled Document</title>
+</head>
+
+<body bgcolor="#fff2e5" alink="#0000FF" vlink="#0000FF">
+
+<?php
+	include "index.php";
+?>
+
+<p><em><font color="#FF9900"><strong><u><font size="7"><font face="Georgia, Times New Roman, Times, serif">Contact Us</font></font></u></strong> </font></em>></p>
+
+<p>
+
+<p><em><font color="#FFCC00"><strong><font color="#FF0066" size="5" face="Georgia, Times New Roman, Times, serif">Connect Publishers Pvt. Ltd .</font></strong></font></em>
+<p><em><strong><font color="#FF0066" size="5" face="Georgia, Times New Roman, Times, serif"> </font></strong></em><font color="#FF0000"></font><font color="#FF0000"></font><font color="#FF0000"></font><font color="#FF0000"></font>
+<font color="#FF0000"><TR></tr>
+</font><font color="#FF0000"><em><strong><font color="#FF9933" size="5" face="Georgia, Times New Roman, Times, serif">Tanya kapil</font></strong></em><strong><font color="#C0C0C0"></br>
+</font></strong></font><font color="#0000"><strong><em><font size="4" face="Georgia, Times New Roman, Times, serif">uma vansi</font></em>  <br>
+<em><font size="4" face="Georgia, Times New Roman, Times, serif">noida(sec 15),</font></em></br> 
+<em><font size="4" face="Georgia, Times New Roman, Times, serif">saharanpur:247001.</font></em> <br>
+<em><font size="4" face="Georgia, Times New Roman, Times, serif">E-mail : riyakapil99@gmail.com</font></em></br> 
+<em><font size="4" face="Georgia, Times New Roman, Times, serif">Mobile :  8272062520</font></em> <br>
+ </strong></font>  
+</p>
+<p align="center">
+</body>
+</html>
---
+++ home.php
@@ -0,0 +1,90 @@
+<html>
+<head>
+	<title>home page</title>
+	<link rel="stylesheet" type="text/css" href="link.html">
+</head>
+<body bgcolor="#fff2e5">
+
+<?php
+	include "index.php";
+?>
+<br>
+<br>
+	<center><font color=chocolate size=4><b><i><u>HOME</u></i></b></font></center>
+	<br>
+
+	<table>
+	<tr>
+		<td>&nbsp;</td>
+		<td><font color="#7c0000"><b><i></i></b></font><center>
+		  <font color="#7c0000"><b><i>HOTEL IN GOA:HOTEL COUPLE'S DESTINATION
+		</i></b></font>
+		</center></td>
+	</tr>
+	<tr>
+		<td><a href=h1.php target=_blank><img src="image/Leela-Hotel-Porters-LARGE.JPG" width=144 height=166></a></td>
+		<td><font color=chocolate>		
+						Hotel Couple's Destination,among the premier
+		business hotel in
+						goa the economic capital of
+		saurashtra is 			
+						characterised by the traditionally
+		hospitality located
+						in the heart of the city.The ideal
+		choice amongs hotels
+						in Goa for businessman and
+		tourist.</font></td>
+		<td><a href=h2.php target=_blank><img src="image/hbusicent.jpg" width=125 height=150></a></td>
+	</tr>
+	<tr>
+		<td><font color="#7c0000">Entry</font></td>
+		<td></td>
+		<td><font color="#7c0000">Reception</font></td>
+	</tr>
+	<tr>
+		<td>&nbsp;</td>
+		<td><font color=darkpink>
+					The Couple's Destination offers facilities that no 
+		other goa hotels
+					offer.Each room is elegantly well
+		appointed and equipped
+					with complete amenities including
+		aircondition fully stock
+					minibars,directIDD telephone,TV
+		with 24 hour satelight
+					entertainment and news.Upscale
+		bathroom with hair dryers
+					and cosmetic mirrors,seperate
+		hanging closets.</font></td>
+		<td></td>
+	</tr>
+	</table>
+		<br><br>
+	<table>
+		<tr>
+			<td><a href=h3.php target=_blank><img src="image/pic5.jpg" width=125 height=150></a></td>
+			<td><font color=green>
+			The only one Goa hotels having all facilities a business center,beauty parlor,health club,gym,saloon,
+			boutique,internet cafe,pastry shop,travel desk and money exchange the Couple's Destination hotels given you full value
+			for money.</font></td>
+			<td><a href=h4.php target=_blank><img src="image/hrooms.jpg" width=125 height=150></a></td>
+		</tr>
+		<tr>
+			<td><font color="#7c0000">Main hall</font></td>
+		<td></td>
+			<td><font color="#7c0000">Lobby</font></td>
+		</tr>
+		<td></td>
+		<td><font color=darkpink>
+		Hotel Couple's Destination takes pride in being a host to a number of celebrities,including bollywood and hollywood stars,
+		cricketers,political leaders as well as foreigners and also being the most prefered in goa.By the NRIs.
+		<td></td>
+		</tr>
+	</table>
+    <marquee behavior=alternate bgcolor="#7e0000"><b><i><a href=contectus.php><font color="white">Devloped By :- Tanya kapil </a></i></b></font></marquee>
+</body>
+</html>
+		
+
+
+
